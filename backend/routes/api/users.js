6babba08eb2c5f8ec93f74c2810d5ff90b7ef658
anti-epic <donahoe.chase@gmail.com>
2022-12-15 16:15:54 -0600

    fetch('/api/users', {   method: 'POST',   headers: {     Content-Type: application/json,     XSRF-TOKEN:   },   body: JSON.stringify({     email: 'spidey@spider.man',     username: 'Spidey',     password: 'password'   }) git commit -m

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,27 @@
 // backend/routes/api/users.js
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-module.exports = router;
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+
+
+
+
+// Sign up
+router.post('/', async (req, res) => {
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user: user
+      });
+    });
+
+
+
+
+    module.exports = router;
