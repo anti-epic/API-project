ff997ac24d0f149f87f0cb8de0207e212c3ee078
anti-epic <donahoe.chase@gmail.com>
2022-12-19 15:27:23 -0600

    setup response code and correct data to be sent back in response body

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
-      const { id, username, email, firstName } = this; // context will be the User instance
-      return { id, username, email, firstName};
+      const { id, firstName, lastName, email, username} = this; // context will be the User instance
+      return { id, firstName, lastName, email, username};
     }
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
