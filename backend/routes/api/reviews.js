136a3a32d532468ed53fd86a5b45789c1737ff69
anti-epic <donahoe.chase@gmail.com>
2022-12-22 16:34:07 -0600

    created get all reviews by current user end point

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const router = express.Router();
+const {requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { Spot, Review, SpotImage, User, ReviewImage } = require('../../db/models');
+
+
+
+
+
+
+
+
+router.get('/', async(req,res,next) =>{
+    res.send('in reviews')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/current', requireAuth, async(req,res,next) =>{
+const {id} = req.user
+console.log(id)
+    const Reviews = await Review.findAll({
+        where: {userid: id},
+        include: {model: ReviewImage}
+
+    })
+    const userInfo = await User.findByPk(id, {
+
+            attributes: ['id', 'firstName', 'lastName']
+
+    })
+
+    let spotsIds = [];
+
+    Reviews.forEach(review => {
+        review.dataValues.User = userInfo
+        spotsIds.push(review.dataValues.spotId)
+
+
+
+    })
+    // console.log(spotsIds)
+let spots
+    for(let i = 0; i < spotsIds.length; i++){
+        let spotId = spotsIds[i]
+        spots = await Spot.findByPk(spotId,
+            {include: [
+                {model: SpotImage,
+                attributes: ['preview', 'url']}
+            ]
+    })
+
+
+    // if(image.dataValues.preview === true){
+    //     spot.dataValues.previewImage = image.url
+    // }
+    // else {
+    //     let none ="no preview image found"
+    //     spot.dataValues.previewImage = none
+    // }
+
+
+
+
+        Reviews[i].dataValues.Spot = spots;
+
+
+
+        Reviews[i].dataValues.Spot.SpotImages.forEach(image =>{
+            console.log('testing', image.dataValues)
+            if(!Reviews[i].dataValues.Spot.dataValues.previewImage){
+             if(image.dataValues.preview === true){
+            Reviews[i].dataValues.Spot.dataValues.previewImage = image.url
+
+    }
+    else {
+        let none ="no preview image found"
+        Reviews[i].dataValues.Spot.dataValues.previewImage = none
+    }
+}
+   delete Reviews[i].dataValues.Spot.dataValues.SpotImages
+   delete Reviews[i].dataValues.Spot.dataValues.createdAt
+   delete Reviews[i].dataValues.Spot.dataValues.updatedAt
+   delete Reviews[i].dataValues.Spot.dataValues.description
+        })
+
+
+
+
+
+
+
+
+
+
+        // console.log(Reviews[i].dataValues.Spot.SpotImages)
+    }
+
+    res.json({Reviews})
+})
+
+
+
+
+
+
+module.exports = router;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const {requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
