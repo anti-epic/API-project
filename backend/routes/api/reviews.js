33e22c9622ba566cf6d87bb65fd052c155ad3064
anti-epic <donahoe.chase@gmail.com>
2022-12-23 10:50:48 -0600

    created a delete review route which checks to make sure the current user made the review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -210,7 +210,36 @@ res.json(updateReview)
 })
 
 
+router.delete('/:reviewId',requireAuth, async(req,res,next)=> {
+    const {reviewId} = req.params;
+    const currUser = req.user.id;
+    console.log(currUser)
+    let review = await Review.findByPk(reviewId)
 
+    if(!review){
+        res.statusCode = 404;
+      return  res.json({
+            "message": "Review couldn't be found",
+            "statusCode": res.statusCode
+        })
+    }
+    const ownerId = review.dataValues.userId
+
+    if(currUser !== ownerId){
+        res.statusCode = 403;
+      return  res.json({
+            message: "Forbidden, you can not delete a review you did not create",
+            statusCode:   res.statusCode
+        })
+    }
+
+    review.destroy();
+    res.statusCode = 200;
+   res.json({
+    message: "Successfully deleted",
+    statusCode: res.statusCode
+   })
+})
 
 
 module.exports = router;
